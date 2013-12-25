ecab93405c4eb5454e1226c00ed610c894fff6ad
Luqi Pan <luqi.pan@gmail.com>
2013-12-25 13:41:09 -0800

    Finished Chap.10-Modules

---
+++ C10-Modules/mymodule.py
@@ -0,0 +1,4 @@
+def sayhi():
+  print('Hi, this is mymodule')
+
+__version__ = '0.1'
---
+++ C10-Modules/using_name.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+  print("Run by itseslf")
+else:
+  print('I am being imported')
+
+
