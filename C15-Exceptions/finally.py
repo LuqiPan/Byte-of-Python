1f2987c89eee80bcb2b027349fe27b849f0e4775
Luqi Pan <luqi.pan@gmail.com>
2013-12-26 21:56:16 -0800

    Finished Chap.15-Exceptions

---
+++ C15-Exceptions/finally.py
@@ -0,0 +1,19 @@
+import time
+
+f = None
+try:
+  f = open("poem.txt")
+  while True:
+    line = f.readline()
+    if len(line) == 0:
+      break
+    print(line, end="", flush=True)
+    time.sleep(2)
+except FileNotFoundError:
+  print("Could not find file poem.txt")
+except KeyboardInterrupt:
+  print("!! You cancelled the reading from the file.")
+finally:
+  if f:
+    f.close()
+  print("(Cleaning up: Closed the file)")
